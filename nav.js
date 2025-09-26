e14486d10d2d69ac3180d6baf3ac39f2b012a95a
erga0131 <60840242+erga0131@users.noreply.github.com>
2025-09-26 15:30:01 +0900

    Add files via upload

--- nav.js
+++ nav.js
@@ -3,9 +3,8 @@ function toggleMenu() {
     navUl.classList.toggle('active');
 }
 
-data = 
+const data =  
 `
-<!--navbarのHTMLコード-->
 <nav>
     <div class="hamburger" onclick="toggleMenu()">☰</div>
     <ul class="navbar">
@@ -13,67 +12,6 @@ data =
         <li><a href="works.html">Works</a></li>
     </ul>
 </nav>
-
-<!--navbarのCSSコード-->
-<style>
-    body {
-    padding-top: 40px;
-    }
-     nav {
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: 100%;
-        background: #39C5BB;
-        opacity: 0.95;
-        padding: 10px 20px;
-        display: flex;
-        height: 26px;
-        z-index: 9999;
-    }
-    /* ハンバーガーメニュー */
-    .hamburger {
-        display: none;
-    }
-    nav ul {
-        list-style: none;
-        margin: 0;
-        padding: 0;
-        display: flex;
-    }
-    nav ul li a {
-        margin: 0 15px;
-        text-decoration: none;
-        color: #FFFFFF;
-        font-size: 18px;
-    }
-
-    @media (max-width: 700px) {
-        nav{
-        height: 40px;
-        }
-        .hamburger {
-        display: block;
-        color: #FFFFFF;
-        font-size: 28px;
-        }
-        nav ul {
-        flex-direction: column;
-        position: absolute;
-        top: 60px;
-        left: 0;
-        width: 100%;
-        background: #39C5BB;
-        opacity: 0.95;
-        display: none;
-        }
-        nav ul.active {
-        display: flex;
-        }
-        nav ul li {
-        margin: 10px 0;
-        }
-    }
-</style>`
+`;
 
 document.getElementById('navbar').innerHTML = data;
